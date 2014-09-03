4d43ae5c136321c70e78df63204ceb587bfa71ae
niceaji <a.ji@daum.net>
2014-09-03 22:39:19 +0900

    news빼고 angular로변경

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var BG_IMAGE_COUNT = 2,
+    random = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    };
+
+
+
+var app = angular.module('Newtab', [])
+    .controller('BgCtrl', function ($scope) {
+        $scope.bg = {
+            backgroundImage: 'url(bg/' + random(1, BG_IMAGE_COUNT) + '.jpg)'
+        }
+    });
+
